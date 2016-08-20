85b556d5c64c6663a7b6e5679ce4412ffa27ba92
pawimis <pawimis@gmail.com>
2016-08-20 13:07:30 +0200

    Update Variables.java

--- src/sample/Variables.java
+++ src/sample/Variables.java
@@ -4,7 +4,7 @@ package sample;
  * Created by pmisi on 12.08.2016.
  */
 public class Variables {
-    public static final String SERVERAPIKEY = "AIzaSyDyMSd1WhP1RKxJsnZekTCHum_WricxL2c";
+    public static final String SERVERAPIKEY = "xxxx";
     public static final String USERDB = "userDb";
     public static final String ORDERDB = "orderDb";
     public static final String CONTACTSDB= "contactsDb";
